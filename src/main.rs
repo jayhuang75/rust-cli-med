c67ada6a9c163dd73f4754a5810082199536e965
wei <info@webdesignmtl.com>
2023-05-02 14:24:38 -0400

    cli app initi

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod utils;
+mod cmd;
+use std::path::Path;
 
 use utils::{config::Config, error::MaskerError};
 use tracing::{info, debug};
@@ -7,8 +9,20 @@ use tracing::{info, debug};
 async fn main() -> Result<(), MaskerError> {
     
     // init the config
-    let config = Config::new().await?;
+    let path = Path::new("conf.yml");
+    let config = Config::new(path).await?;
+
+    // init the tracing for logging
     config.tracing().await;
+
+    // cmd load
+    // design:
+    // masker 
+    // --file_path= [required]
+    // --file_type= [csv,json] [required] 
+    // --conf_path=conf.yml [optional] default ./conf.yml
+    // --action=[mask,encrypt,decrypt] [optional] default is mask
+
 
     info!("test");
     debug!("debug");
