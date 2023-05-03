ff6986f63a5d1da953869d6a1b636c159477e51d
wei <info@webdesignmtl.com>
2023-05-02 22:34:10 -0400

    wip

--- src/cmd/cli.rs
+++ src/cmd/cli.rs
@@ -1,10 +1,17 @@
+use csv::StringRecord;
+use serde_json::Value;
+use std::path::PathBuf;
+use clap::{arg, command, value_parser, ArgAction, Command};
+
 pub enum FileType {
     CSV,
-    JSON
+    JSON,
 }
 
 impl Default for FileType {
-    fn default() -> Self { FileType::CSV }
+    fn default() -> Self {
+        FileType::CSV
+    }
 }
 
 pub enum Action {
@@ -14,16 +21,59 @@ pub enum Action {
 }
 
 impl Default for Action {
-    fn default() -> Self { Action::MASK }
+    fn default() -> Self {
+        Action::MASK
+    }
 }
 
-pub struct App{
+pub struct CliApp {
     pub file_path: String,
     pub file_type: FileType,
     pub conf_path: String,
-    pub action: Action
+    pub output_path: String,
+    pub action: Action,
 }
 
-impl App {
-    
+impl CliApp {
+
+    pub async fn new() -> Self {
+        let matches = command!() // requires `cargo` feature
+            .arg(
+                arg!(
+                    -c --config_path <FILE> "Sets a custom config yaml path"
+                )
+                .required(true)
+                .default_value("./conf.yml")
+                .value_parser(value_parser!(PathBuf)),
+            )
+            .arg(
+                arg!(
+                    -f --file_path <FILE> "Sets a file/directory path"
+                )
+                .required(true)
+                .value_parser(value_parser!(PathBuf)),
+            )
+            .arg(
+                arg!(
+                    -o --output_path <FILE> "Sets a file/directory path for output"
+                )
+                .required(true)
+                .default_value("./out")
+                .value_parser(value_parser!(PathBuf)),
+            )
+            .arg(
+                arg!(
+                    -t --file_type <FILE_TYPE> "Sets a process file type"
+                )
+                .required(true)
+                .default_value(FileType)
+            )
+            .get_matches();
+
+        if let Some(config_path) = matches.get_one::<PathBuf>("config") {
+            println!("Value for config: {}", config_path.display());
+
+        }
+
+    }
 }
--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,1 +1,2 @@
 pub mod cli;
+mod csv;
