c14b0554a006e1066cf1eeea03edc0ee157aaa38
wei <info@webdesignmtl.com>
2023-05-12 17:54:24 -0400

    update audit metadata

--- audit/migrations/20230512195802_audit_sqlite_datastore.up.sql
+++ audit/migrations/20230512195802_audit_sqlite_datastore.up.sql
@@ -1,7 +1,10 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS audit (
     id INTEGER PRIMARY KEY,
-    item TEXT NOT NULL,
+    total_files INTEGER NOT NULL,
+    total_records INTEGER NOT NULL,
+    runtime_conf TEXT NOT NULL,
+    failure_reason TEXT,
     successed BOOLEAN NOT NULL DEFAULT FALSE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
