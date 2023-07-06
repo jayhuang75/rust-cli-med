ad81ec49a20bf7eb462b8368785a3dad0f44bb10
wei <info@webdesignmtl.com>
2023-07-06 16:35:23 -0400

    update brew build

--- build/brew/brew_tap.sh
+++ build/brew/brew_tap.sh
@@ -5,13 +5,11 @@ curl https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.2/macos_x8
 ls -la
 
 echo ">>> shasum"
-SHASUM=$(shasum -a 256 macos_x86_archive-0.6.2.tar.gz)
+SHASUM=$(shasum -a 256 macos_x86_archive-0.6.2.tar.gz | awk '{printf $1}')
 
 echo ">>> brew tap"
 brew tap jayhuang75/med
 
 echo ">>> brew bump formula pr"
-brew bump-formula-pr -f --version=0.6.2 --no-browse --no-audit \
---sha256=${SHASUM} \
---url="https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.2/macos_x86_archive-0.6.2.tar.gz" \
-jayhuang75/med
+echo ${SHASUM}
+brew bump-formula-pr -v --version=0.6.2 --no-browse --no-audit --sha256="${SHASUM}" --url="https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.2/macos_x86_archive-0.6.2.tar.gz" jayhuang75/med/med
